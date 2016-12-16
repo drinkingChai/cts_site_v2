6b39a1c083792f485ab968fca923eebc95c33d8d
Wasif Zaman <teatocode@gmail.com>
2016-12-15 23:08:21 -0500

    Add color to erate and about

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
-var menuAndOverlay = function() {
-  $("nav.v-nav").toggleClass("slide-right");
-  $(".side-menu-icon").toggleClass("fa-bars fa-times");
-  $(".overlay").toggleClass("overlay-visibility");
-}
-
-$(".side-menu-icon, .overlay").click(function() {
-  menuAndOverlay();
+// snippet from: http://stackoverflow.com/questions/4326845/how-can-i-determine-the-direction-of-a-jquery-scroll-event
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+       // downscroll code
+       $("nav").fadeOut();
+   } else {
+      // upscroll code
+      $("nav").fadeIn();
+   }
+   lastScrollTop = st;
 });
